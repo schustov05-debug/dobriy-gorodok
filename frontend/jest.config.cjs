bea1b582fe528e25aa320fafcc3d661c59716f50
DenisShustov <schustov05@icloud.com>
2026-07-02 03:04:02 +0300

    update site

--- frontend/jest.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
-  moduleNameMapper: {
-    "\\.(css|scss)$": "identity-obj-proxy",
-  },
-};
-
-
